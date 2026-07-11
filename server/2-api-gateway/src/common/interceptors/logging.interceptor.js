99e47d7e6611cd8f6fe13981875073c5b6f034d0
arijit547 <arijitpaul547@gmail.com>
2026-07-11 21:01:14 +0600

    Prompt: Add a logging interceptor to track API request and response times.

---
+++ server/2-api-gateway/src/common/interceptors/logging.interceptor.js
@@ -0,0 +1,7 @@
+export const loggingInterceptor = (req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.path} ${res.statusCode} ${Date.now() - start}ms`);
+  });
+  next();
+};
